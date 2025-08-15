a30b555dbc4351836e3aceae72049141f744149e
vanyazaov <vanyazaov@gmail.com>
2025-08-15 13:06:01 +0300

    Создан шаблон-класса для обработки авторизации - Login с конструктором, 3-мя публичными методами для обработки авторизации, успеха и ошибок авторизации, а также приватный метод для валидации данных.

--- app/includes/login.php
+++ app/includes/login.php
@@ -1,1 +1,11 @@
+<?php header("Content-Type: text/html;charset=utf-8");?>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
 <?php
+require_once('database.php');
+class Login {
+    public function __construct() {}
+    public function index() {}
+    public function loginSuccess() {}
+    public function loginFail() {}
+    private function validateDetails() {}
+}
