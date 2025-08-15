a4213933586e0f8fb53e3a816cdc0bea501e8e9e
vanyazaov <vanyazaov@gmail.com>
2025-08-15 13:51:13 +0300

    Файл, предназначенный для работы панели администратора, наполнен классами без реализации для дальнейшего их наполнения. В файле обозначены  три класса: базовый - Adminpanel и производные от него Posts и Comments.

--- app/includes/admin.php
+++ app/includes/admin.php
@@ -1,1 +1,30 @@
+<?php header("Content-Type: text/html;charset=utf-8"); ?>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
 <?php
+session_start();
+require_once('database.php');
+
+class Adminpanel {
+    public function __construct();
+}
+
+class Posts extends Adminpanel {
+    public function __construct() {
+        parent::__construct();
+    }
+    public function listPosts() {}
+    public function editPosts() {}
+    public function addPost() {}
+    public function savePost() {}
+    public function deletePost() {}
+}
+
+class Comments extends Adminpanel {
+    public function __construct() {
+        parent::__construct();
+    }
+    public function listComments() {}
+    public function deleteComment() {}
+}
+
+$admin = new Adminpanel();
