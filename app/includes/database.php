2028ce3062f96dbb0ac8fcb9fdadd48c0a9d97fd
vanyazaov <vanyazaov@gmail.com>
2025-08-15 12:30:05 +0300

    Создан класс для работы с базой данных. Сразу бросается в глаза отсутствие инкапсуляции и возможность создавать много соединений с БД, а также отсутствие обработки ошибок. В классе присутствует конструктор для создания соединения, а также 3 метода для выбора, добавления и обновления записей. Пока эти методы без реализации.

--- app/includes/database.php
+++ app/includes/database.php
@@ -1,1 +1,22 @@
+<?php header("Content-Type: text/html;charset=utf-8"); ?>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 <?php
+class Database {
+    public $dbserver = '';
+    public $username = '';
+    public $password = '';
+    public $database = '';
+    public $db = '';
+    
+    public function __construct() {
+        $this->dbserver = 'db'; // Хост базы данных. В рамках окружения докера сервис имеет имя "db"
+        $this->username = 'docker'; // Логин пользователя базы данных
+        $this->password = 'password'; // Пароль пользователя базы данных
+        $this->database = 'kickstartblog'; // Название базы данных
+        $this->db = new PDO("mysql://host=".$this->dbserver.";dbname=".$this->database, $this->username, $this->password);
+    }
+    
+    public function dbselect($table, $select, $where = NULL) {}
+    public function dbadd($tablename, $insert, $format) {}
+    public function dbupdate($table, $insert, $where) {}
+}
