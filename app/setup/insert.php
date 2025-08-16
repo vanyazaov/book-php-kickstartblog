d8cb4266916e78c0f842c873d595c762d6645251
vanyazaov <vanyazaov@gmail.com>
2025-08-16 04:09:12 +0300

    Изменены скрипты установки: добавлено создание таблицы для постов и сделан запрос на вставку первого поста.

--- app/setup/insert.php
+++ app/setup/insert.php
@@ -4,6 +4,7 @@ $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 try {
 	$queryStr = "INSERT INTO users (name, password, email) VALUES ('admin', MD5('admin'), 'youremail@domain.com')";
 	$db->query($queryStr);
+	$queryStr = "INSERT INTO posts (title, content) VALUES ('Hello, World!', 'Hello!\r\nThis is my first post!');";
 } catch (PDOException $e) {
 	echo $e->getMessage();
 }
--- app/setup/setup.php
+++ app/setup/setup.php
@@ -4,6 +4,8 @@ $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 try {
 	$queryStr = "CREATE TABLE users (id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(40), password VARCHAR(100), email VARCHAR(150))";
 	$db->query($queryStr);
+	$queryStr = "CREATE TABLE posts (id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255), content TEXT)";
+	$db->query($queryStr);
 } catch (PDOException $e) {
 	echo $e->getMessage();
 }
