b083b03c1c05a18e7862a54aab2d601a300d0b99
vanyazaov <vanyazaov@gmail.com>
2025-08-16 22:28:09 +0300

    Исправлен установочный скрипт: в проекте, для шифрования пароля вместо MD5 используется функция crypt с солью.

--- app/setup/insert.php
+++ app/setup/insert.php
@@ -2,7 +2,8 @@
 $db = new PDO("mysql:host=db;dbname=kickstartblog", "docker", "password");
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 try {
-	$queryStr = "INSERT INTO users (name, password, email) VALUES ('admin', MD5('admin'), 'youremail@domain.com')";
+	$password = crypt('admin', '$2a$07$R.gJb2U2N.FmZ4hPp1y2CN$');
+	$queryStr = "INSERT INTO users (name, password, email) VALUES ('admin', '$password', 'youremail@domain.com')";
 	$db->query($queryStr);
 	$queryStr = "INSERT INTO posts (title, content) VALUES ('Hello, World!', 'Hello!\r\nThis is my first post!');";
 } catch (PDOException $e) {
